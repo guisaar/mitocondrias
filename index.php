79513aad6055af939fce7fdebbc8a84c091ff7c8
guisaar <56200519+guisaar@users.noreply.github.com>
2019-11-20 09:33:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,738 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!--Gif Screen Recorder -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portifolio</title>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:900,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="style.css"> <link rel="stylesheet" type="text/css" href="ScriptCine.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://github.com/defunkt/jquery-pjax">
+</script>
+
+<script type="text/javascript">
+var stateObj = { foo: "bar" };
+function change_my_url()
+{
+   history.pushState(stateObj, "page 2", "bar.html");
+}
+var link = document.getElementById('click');
+link.addEventListener('click', change_my_url, false);
+</script>
+
+</head>
+<body class="avadaKedabra">
+
+  <header class="header-paralax">
+    <h1>Olá, Somos a <font>Mitocondria</font>.<br> nós somos uma parte de você.
+	<div id="Mywork" onclick="quantotestemeu()">Veja Sobre<img src="setadireta2.png" style="width:auto;margin-left:5%;margin-top:auto;max-height:20px;" id="imgwork"> </div>
+	
+	</h1>
+
+  </header>
+   
+   
+  
+   <nav class="menu2">
+	<ul>
+<li><a onclick="sobre()" href="#" class="active3">Sobre </a></li>
+    <li><a onclick="index()" href="#" class="active3" id="menuimg" >Banco de Dados</a></li>
+  
+  <li><a onclick="Mitocondrias()" href="#" class="active3" >Mitocondrias</a></li>
+  
+   
+   
+
+  
+
+</ul>
+
+</nav>
+
+
+
+
+  <main>
+  <nav class="menu">
+	<ul>
+<li><a onclick="sobre()" href="#" class="active3">Sobre </a></li>
+    <li><a onclick="index()" href="#" class="active3" id="menuimg" >Banco de Dados</a></li>
+  
+  <li><a onclick="Mitocondrias()" href="#" class="active3" >Mitocondrias</a></li>
+</ul>
+
+</nav>
+    <div class="content" id="Index" style="display:none">
+
+  
+
+  
+  
+   <section id="secao2" class="slide">
+<div><h1 class="Texto313" style="text-align:center">Projetos</h1><br></div>
+  
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="mitoc.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Mitocôndrias <a href="#" id='click'>Click to change url to bar.html</a></p><br>
+ Um trabalho Interdisciplinar realizados nas aulas de banco de dados, onde se foi dividido por meio de um “leilão” de cada uma das organelas presentes nas células. Foi realizado em grupo onde se foi feito um banco de dados da organela, mostrando oque está contido e oque contem a organela. 
+ 
+	
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+
+
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="shell script.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Shell Script - RPG </p><br>
+
+Um projeto da matéria de fundamentos de informática, onde está sendo feito uma lista de comandos “Linux” para entender mais sobre esses conceitos, como um mini projeto estou fazendo uma saga RPG, que está sendo iniciada no shell script, onde se foi feito uma introdução ao próximo jogo.
+
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+     <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="starbucks.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Starbucks – Roteiro Reunião	</p><br>
+	
+
+Esse foi um trabalho escolar com principal objetivo saber se portar em uma reunião. Foi feito sobre tema livre, nosso grupo fez sobre uma empresa de café que queria derrubar sua concorrente. Se foi feito um roteiro português/inglês. 
+
+
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+  </section>
+  <section id="secao2" class="slide">
+
+
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="gitku2.png"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  <p>Projetos online com Github e Heroku </p><br>
+Aprendi que melhor que ficar abrindo pasta a pasta, é muito mais fácil realizar isso apenas colocando apenas um link.
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+     <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="pastoral .jpg"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	 <p>Projeto Social</p><br>
+Frequentemente realizo um serviço social na pastoral dos coroinhas e cerimoniarios na paroquia são Paulo apostolo no zaira. 
+
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+  </section>
+   
+  
+  
+
+
+  
+
+  
+    	
+  
+  
+  
+  
+    <section id="secao21" class="slide">
+
+   
+<footer id="rodape" class="text" >
+		<h1 class="Texto" style="text-align:center" id="contato">Contato</h1><br>
+<div style="">
+
+
+ Desenvolvido por:Guilherme Saar <br>
+  <br> 
+ Proibido a distribuição ou reprodução do produto sem autorização<br>
+  <br> 
+<a href="https://www.facebook.com/guilherme.saar.7" class="fa fa-facebook"></a> Facebook <br> <br>
+
+<a href="#" class="fa fa-google"></a> Gmail<font style="visibility:hidden">ook</font> <br>
+</div>
+	
+<div style="">	
+
+
+<form action="Formulario.php"><fieldset id="dadosUsuario" style="border:none;text-align:center;">
+    <fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Email</legend>
+   
+ 
+  <input type="email" name="EmailRetorno" id="SEmail"maxlength="80" placeholder="MickeyMouse@gmail.com"> <br> 
+  <br>    
+<fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Mensagem</legend>
+  
+  <TextArea name="Mensagem" id="Sassu" cols="60" rows="10" placeholder="Coloque sua mensagem aqui."></textarea> <br> 
+    <br>  
+  </fieldSet>
+
+  <button onclick="Senvia()" ><a href="indes.html" style="text-decoration:none;">Enviar</a></button> <button onclick="Sancelar" ><a href="" style="text-decoration:none;">Cancelar</a></button> 
+
+	 
+
+	
+
+</div>
+	</footer> 
+
+
+  </section>
+
+
+
+	
+	
+	
+	
+	
+	
+	 
+	 </div>
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	  <div class="content" id="Mitocondrias" style="display:none;">
+
+  
+  
+  <section id="secao2" class="slide">
+<div><h1 class="Texto313" style="text-align:center">Projetos</h1><br></div>
+  
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="mitoc.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Mitocôndrias </p><br>
+ Um trabalho Interdisciplinar realizados nas aulas de banco de dados, onde se foi dividido por meio de um “leilão” de cada uma das organelas presentes nas células. Foi realizado em grupo onde se foi feito um banco de dados da organela, mostrando oque está contido e oque contem a organela. 
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+
+
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="shell script.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Shell Script - RPG </p><br>
+
+Um projeto da matéria de fundamentos de informática, onde está sendo feito uma lista de comandos “Linux” para entender mais sobre esses conceitos, como um mini projeto estou fazendo uma saga RPG, que está sendo iniciada no shell script, onde se foi feito uma introdução ao próximo jogo.
+
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+     <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="starbucks.gif"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	  <p>Starbucks – Roteiro Reunião	</p><br>
+	
+
+Esse foi um trabalho escolar com principal objetivo saber se portar em uma reunião. Foi feito sobre tema livre, nosso grupo fez sobre uma empresa de café que queria derrubar sua concorrente. Se foi feito um roteiro português/inglês. 
+
+
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+  </section>
+  <section id="secao2" class="slide">
+
+
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="gitku2.png"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  <p>Projetos online com Github e Heroku </p><br>
+Aprendi que melhor que ficar abrindo pasta a pasta, é muito mais fácil realizar isso apenas colocando apenas um link.
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+     <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_11" >
+	<div id="sobre">
+	 <div class="div1111"><img src="pastoral .jpg"></div>
+	  <div class="div2111">
+	  
+	 
+	  
+	  
+	 <p>Projeto Social</p><br>
+Frequentemente realizo um serviço social na pastoral dos coroinhas e cerimoniarios na paroquia são Paulo apostolo no zaira. 
+
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  
+
+  </section>
+   
+  
+  
+
+
+  
+
+  
+  
+
+  
+    	
+  
+  
+  
+  
+    <section id="secao21" class="slide">
+
+   
+<footer id="rodape" class="text" >
+		<h1 class="Texto" style="text-align:center" id="contato">Contato</h1><br>
+<div style="">
+
+
+ Desenvolvido por:Guilherme Saar <br>
+  <br> 
+ Proibido a distribuição ou reprodução do produto sem autorização<br>
+  <br> 
+<a href="https://www.facebook.com/guilherme.saar.7" class="fa fa-facebook"></a> Facebook <br> <br>
+
+<a href="#" class="fa fa-google"></a> Gmail<font style="visibility:hidden">ook</font> <br>
+</div>
+	
+<div style="">	
+
+
+<form action="Formulario.php"><fieldset id="dadosUsuario" style="border:none;text-align:center;">
+    <fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Email</legend>
+   
+ 
+  <input type="email" name="EmailRetorno" id="SEmail"maxlength="80" placeholder="MickeyMouse@gmail.com"> <br> 
+  <br>    
+<fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Mensagem</legend>
+  
+  <TextArea name="Mensagem" id="Sassu" cols="60" rows="10" placeholder="Coloque sua mensagem aqui."></textarea> <br> 
+    <br>  
+  </fieldSet>
+
+  <button onclick="Senvia()" ><a href="indes.html" style="text-decoration:none;">Enviar</a></button> <button onclick="Sancelar" ><a href="" style="text-decoration:none;">Cancelar</a></button> 
+
+	 
+
+	
+
+</div>
+	</footer> 
+
+
+  </section>
+
+
+
+	
+	
+	
+	
+	
+	
+	 
+	 </div>
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	     <div class="content" id="sobrehtml" style="display:block">
+
+  
+
+  
+
+
+	  
+ <section id="secao2" class="slide">
+<div><h1 class="Texto313" style="text-align:center" id="pt-s">Sobre</h1><br></div>
+
+   <section id="secao1322" class="slide" >
+	
+
+	 
+	 <aside id="lateral20_112" >
+	<div id="sobre">
+	 <div class="div1111"><img src="Logofinal.jpg"></div>
+	  <div class="div2111">
+	  
+	 
+	  <p>História e objetivo</p><br>
+	
+Nasci em 2004, atualmente tenho 15 anos, estudei na toca do espertinho, Francisca Lopes Negri, e também no “Viscondão”, ou Therezinha Sartori, Estudo atualmente na etec de Rio grande da Serra, fiz alguns cursinhos.
+Faço cursos pela internet, atualmente um de PHP.
+Meu principal objetivo é me formar na área de Programação, especificamente desenvolvimento de software.
+
+ 
+	 
+	  
+	  </div>
+	 </div>  
+
+	</aside>
+  </section>
+  
+  
+  <section id="secao1322" class="slide" >
+	
+
+	 <aside id="lateral20_1" >
+	<div class="div3" style="background-image: url('Logofinal3.jpg');background-size:100% 100%;padding:0;width:100%;height:100%;"id="teste12" >
+	
+	<div class="div3" style="background:#f7c27500;padding:0;width:100%;height:100%;" >
+
+<div class="div51" style="visibility:hidden;"><a href="" >Saiba Mais</a></div>	
+<div class="div51" style="visibility:hidden;"><a href="" >Saiba Mais</a></div>
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>	
+<div class="div51" >Curiosidade <br> Meu nome. </div>
+
+<br><br><br>	
+
+</div>
+	
+
+
+</div>
+
+	</aside>
+	 <aside id="lateral20_1" >
+	<div class="div3" style="background-image: url('Logofinal3.jpg');background-size:100% 100%;padding:0;width:100%;height:100%;"id="teste12" >
+	
+	<div class="div3" style="background:#f7c27500;padding:0;width:100%;height:100%;" >
+
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>	
+<div class="div51" style="visibility:hidden"><a href="" >Saiba Mais</a></div>	
+<div class="div51">Filosofia de vida<br>Ser feliz para fazer os outros felizes.</div>
+
+<br><br><br>	
+
+</div>
+	
+
+
+</div>
+
+	</aside>
+  </section>
+  
+  
+  
+
+  </section>
+  
+  
+
+ 
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+    	
+  
+  
+  
+  
+    <section id="secao21" class="slide">
+
+   
+<footer id="rodape" class="text" >
+		<h1 class="Texto" style="text-align:center" id="contato">Contato</h1><br>
+<div style="">
+
+
+ Desenvolvido por:Guilherme Saar <br>
+  <br> 
+ Proibido a distribuição ou reprodução do produto sem autorização<br>
+  <br> 
+<a href="https://www.facebook.com/guilherme.saar.7" class="fa fa-facebook"></a> Facebook <br> <br>
+
+<a href="#" class="fa fa-google"></a> Gmail<font style="visibility:hidden">ook</font> <br>
+</div>
+	
+<div style="">	
+
+
+<form action="Formulario.php"><fieldset id="dadosUsuario" style="border:none;text-align:center;">
+    <fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Email</legend>
+   
+ 
+  <input type="email" name="EmailRetorno" id="SEmail"maxlength="80" placeholder="MickeyMouse@gmail.com"> <br> 
+  <br>    
+<fieldset id="Avaliacao" style="border-left:none;border-right:none;border-bottom:none"><legend>Mensagem</legend>
+  
+  <TextArea name="Mensagem" id="Sassu" cols="60" rows="10" placeholder="Coloque sua mensagem aqui."></textarea> <br> 
+    <br>  
+  </fieldSet>
+
+  <button onclick="Senvia()" ><a href="indes.html" style="text-decoration:none;">Enviar</a></button> <button onclick="Sancelar" ><a href="" style="text-decoration:none;">Cancelar</a></button> 
+
+	 
+
+	
+
+</div>
+	</footer> 
+
+
+  </section>
+
+
+
+	
+	
+	
+	
+	
+	
+	 
+	 </div>
+  </main>
+  
+<script type="text/javascript" src="script2.js"></script>
+</body>
+</html>
